ae70bb00cf7887fe20a10bd14f43dae57ca4597a
Vitaly <dehame@gmail.com>
2019-07-07 20:33:15 +0300

    commit3

--- py/AnotherTestFile.py
+++ py/AnotherTestFile.py
@@ -1,4 +1,20 @@
-#Some File
-another test of another push1
-another test of another push2
-another test of another push3
+import sys
+import io
+
+this_text = io.StringIO()
+
+print('before')
+saved_output = sys.stdout
+sys.stdout = this_text
+import this
+sys.stdout = saved_output
+print('after')
+full_this_text = this_text.getvalue()
+full_this_text = full_this_text.splitlines()
+this_text.close()
+for i,line in enumerate(full_this_text):
+    print(i,end=' ')
+    txt_file = open('py_zen_' + str(i),'w')
+    txt_file.write(line)
+    txt_file.close()
+
